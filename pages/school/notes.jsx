cb5a9e2e83b0d4c5111264b1125ff15349fda78c
MonkyMars <155177968+MonkyMars@users.noreply.github.com>
2024-09-09 17:46:05 +0200

    Added notes

---
+++ pages/school/notes.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import Nav from '/components/Nav';
+import styles from '/styles/Notes.module.css';
+import Image from 'next/image';
+
+const Notes = () => {
+  const [note, setNote] = useState({
+    title: '',
+    description: '',
+    date: '',
+    time: '',
+  })
+  const [existingNotes, setExistingNotes] = useState([{id: 0, title: 'Note test'}])
+  const insertNote = () => {
+
+  }
+
+    return(
+      <>
+        <Nav/>
+        <main className={styles.Main}>
+          <div className={styles.Topbar}>
+            <input type='text' placeholder='Enter note title' value={note.title} onChange={(e) => setNote({title: e.target.value})}/>
+            {note.title && <button onClick={insertNote}><Image src={'/send.png'} width={30} height={30}/></button>}
+          </div>
+          <div className={styles.noteContainer}>
+            {existingNotes.map((note, index) => {
+              return(
+                <Note key={note.id || index} title={note.title}/>
+              )
+            })}
+          </div>
+        </main>
+      </>
+    )
+}
+
+export default Notes;
+
+const Note = ({title, description, time, date}) => {
+  return(
+    <div className={styles.Note}>
+      <header>
+        <h2>{title}</h2>
+      </header>
+      <main>
+        <p>{description}</p>
+      </main>
+      <footer>
+        <label>{date}, {time}</label>
+      </footer>
+    </div>
+  )
+}
