82251a4c1520436c017cbaec26693048dc78c6ce
MonkyMars <155177968+MonkyMars@users.noreply.github.com>
2024-09-10 19:53:44 +0200

    refined nav

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,34 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from '../styles/components/Nav.module.css';
-import { useMediaQuery } from 'react-responsive'
+import { useMediaQuery } from 'react-responsive';
 
 export default function Nav() {
-  const isDesktopOrLaptop = useMediaQuery({
-    query: '(min-width: 1224px)'
-  });
-    return(
-      <>
-        {isDesktopOrLaptop ? <NavDesktop/> : <NavPhone/>}
-      </>
-    )
+    const isDesktopOrLaptop = useMediaQuery({
+        query: '(min-width: 1224px)'
+    });
+
+    return (
+        <>
+            {isDesktopOrLaptop ? <NavDesktop /> : <NavPhone />}
+        </>
+    );
 }
 
 export const NavDesktop = () => {
-    return(
-      <>
-        <nav className={styles.Nav}>
-          <h1>DailySchool</h1>
-        </nav>
-      </>
-    )
+    return (
+      <nav className={styles.Nav}>
+            <h1>DailySchool</h1>
+      </nav>
+    );
 }
 
 export const NavPhone = () => {
-    return(
-      <>
-      <nav className={styles.Nav}>
-        <h1>DailySchool</h1>
-      </nav>
-      </>
-    )
+    return (
+        <nav className={styles.Nav}>
+            <h1>DailySchool</h1>
+        </nav>
+    );
 }
