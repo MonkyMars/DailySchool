4f8754f2cbad9cbc17de4953aef9038fec1e9727
MonkyMars <155177968+MonkyMars@users.noreply.github.com>
2024-09-12 19:34:43 +0200

    added placeholder

--- pages/api/updateUser.js
+++ pages/api/updateUser.js
@@ -1,6 +1,11 @@
-import bcrypt from 'bcrypt';
-import { sql } from '@vercel/postgres';
+import bcrypt from "bcrypt";
+import { sql } from "@vercel/postgres";
 
-export default async function updateUser() {
-    
+export default async function updateUser(res, req) {
+  if (req.method == "POST") {
+    try {
+    } catch (err) {
+      console.err(err);
+    }
+  }
 }
