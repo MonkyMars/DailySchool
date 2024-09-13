ae2c4a25fa96d4a41d04d7cadfc610acc4e4a013
MonkyMars <155177968+MonkyMars@users.noreply.github.com>
2024-09-13 13:50:29 +0200

    improved and added backend

--- pages/api/addPlanner.js
+++ pages/api/addPlanner.js
@@ -1,28 +1,25 @@
 import { sql } from "@vercel/postgres";
 
-export default async function AddNote(req, res) {
-    if (req.method === 'POST') {
-        try {
+export default async function addPlanner(req, res) {
+    if(req.method === 'POST') {
+        try{
             const { title, description, date, time } = req.body;
             const user = JSON.parse(req.headers['user']);
 
             if (!user || !user.id) {
                 return res.status(400).json({ error: "User information is missing" });
             }
-            const [day, month, year] = date.split('/');
-            const formattedDate = `${year}-${month}-${day}`
             const result = await sql`
-                INSERT INTO notes (title, description, date, time, "user")
-                VALUES (${title}, ${description}, ${formattedDate}, ${time}, ${user.id});
+                INSERT INTO planner (title, description, date, time, "user")
+                VALUES (${title}, ${description}, ${date}, ${time}, ${user.id});
             `;
-
             res.status(200).json({ message: "Note added successfully", result });
-        } catch (err) {
-            console.error(err);
+        } catch(error) {
+            console.error(error);
             res.status(500).json({ error: err.message });
         }
     } else {
         res.setHeader("Allow", ["POST"]);
         res.status(405).json({ error: `Method ${req.method} Not Allowed` });
     }
-}
+} 
