cad2b13de1d8eb4411e669979b5254a650dc3e7c
LeviNoppers <levinoppers69@gmail.com>
2024-09-14 22:45:43 +0200

    updated packages

--- pages/api/updateUser.js
+++ pages/api/updateUser.js
@@ -1,7 +1,7 @@
 import bcrypt from "bcrypt";
 import { sql } from "@vercel/postgres";
 
-export default async function updateUser(res, req) {
+export default async function updateUser(req, res) {
   if (req.method == "POST") {
     try {
     } catch (err) {
