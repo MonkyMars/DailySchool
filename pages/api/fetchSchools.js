4be54f0c3b841cb2dcd338b666ffdba1cb70f9cb
LeviNoppers <155177968+MonkyMars@users.noreply.github.com>
2024-09-20 15:51:33 +0200

    Update fetchSchools.js

--- pages/api/fetchSchools.js
+++ pages/api/fetchSchools.js
@@ -4,8 +4,8 @@ import { sql } from "@vercel/postgres";
 export default async function fetchSchools(req, res) {
   if (req.method === "GET") {
     try {
-      const { rows: schools } = await sql`SELECT * FROM "School"`;
-      res.status(200).json({ message: schools });
+      const result = await sql`SELECT * FROM "School"`;
+      res.status(200).json({ message: result });
     } catch (err) {
       console.error("Error fetching schools:", err);
       res.status(500).json({ error: "Failed to fetch schools" });
