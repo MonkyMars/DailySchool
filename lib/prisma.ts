e64525ac362553d6b5c02eb62bed5b2add0f74ed
MonkyMars <155177968+MonkyMars@users.noreply.github.com>
2024-09-20 17:57:11 +0200

    redid prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,8 +3,6 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = globalThis.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") {
-  globalThis.prisma = prisma;
-}
+globalThis.prisma = prisma;
 
 export default prisma;
