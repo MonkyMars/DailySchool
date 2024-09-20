58b792466bd3fc5c13334813ea7dda36bd39c16a
MonkyMars <155177968+MonkyMars@users.noreply.github.com>
2024-09-20 18:05:03 +0200

    added generate script to package.json for vercel

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client"; // Import User type
 import CredentialsProvider from "next-auth/providers/credentials";
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
 
